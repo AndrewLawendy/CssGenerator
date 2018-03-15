bfcae2a40f3e782a9507a94d57c2f3f2807d98eb
Andrew Gorgui <Andrew.Gorgui@linkdev.com>
2018-03-15 16:33:08 +0200

    Working with box model

--- js/main.js
+++ js/main.js
@@ -1,3 +1,61 @@
+var cssGetter = function (prop, value, unit) {
+    var cssDeclaration = {
+        height: 'height:',
+        minHeight: 'min-height:',
+        width: 'width:',
+        minWidth: 'min-width:',
+        padding: 'padding:',
+        paddingTop: 'padding-top:',
+        paddingRight: 'padding-right:',
+        paddingBottom: 'padding-bottom:',
+        paddingLeft: 'padding-left:',
+        margin: 'margin:',
+        marginTop: 'margin-top:',
+        marginRight: 'margin-right:',
+        marginBottom: 'margin-bottom:',
+        marginLeft: 'margin-left:',
+        border: 'border:',
+        borderWidth: 'border-width',
+        borderTopWidth: 'border-top-width:',
+        borderRightWidth: 'border-right-width:',
+        borderBottomWidth: 'border-bottom-width:',
+        borderLeftWidth: 'border-left-width:',
+        borderColor: 'border-color',
+        borderTopColor: 'border-top-color',
+        borderRightColor: 'border-right-color',
+        borderBottomColor: 'border-bottom-color',
+        borderLeftColor: 'border-left-color',
+        borderStyle: 'border-style',
+        borderTopStyle: 'border-top-style',
+        borderRightStyle: 'border-right-style',
+        borderBottomStyle: 'border-bottom-style',
+        borderLeftStyle: 'border-left-style',
+        borderRadius: '-webkit-border-radius:;border-radius:',
+        borderTopRightRadius: '-webkit-border-top-right-radius:;border-top-right-radius:',
+        borderBottomRightRadius: '-webkit-border-bottom-right-radius:;border-bottom-right-radius:',
+        borderBottomLeftRadius: '-webkit-border-bottom-left-radius:;border-bottom-left-radius:',
+        borderTopLeftRadius: '-webkit-border-top-left-radius:;border-top-left-radius:',
+        boxShadow: '-webkit-box-shadow:;box-shadow:',
+        position: 'position',
+        top: 'top',
+        right: 'right',
+        bottom: 'bottom',
+        left: 'left',
+        zIndex: 'z-index',
+        display: 'display',
+        boxSizing: '-webkit-box-sizing:;box-sizing:',
+        overflow: 'overflow',
+        overflowX: 'overflow-x',
+        overflowY: 'overflow-y',
+        float: 'float',
+        clear: 'clear',
+        opacity: 'opacity'
+    };
+    return cssDeclaration[prop].split(';').map(function (x) {
+        return x + value + unit
+    }).join(';');
+}
+
 var initCssGenerator = function () {
     $('#collapseAll').on('click', function () {
         $('.collapsible').removeClass('active');
@@ -9,7 +67,12 @@ var initCssGenerator = function () {
         $('.collapsible-content').slideDown('fast');
     });
 
-    $('.prop-control .all input').on('change', function () {
+    $('#reset').on('click',function(){
+        $('.prop-control input').val('');
+        $('.prop-control select').prop('selectedIndex',0);
+    });
+
+    $('.prop-control [class*="switch"] input[data-show]').on('change', function () {
         var dataShow = $(this).data('show');
         $('#' + dataShow).slideToggle('fast');
     });
@@ -20,8 +83,15 @@ var initCssGenerator = function () {
             colorValue = $(this).val();
         propUnit.text(colorValue).css('background', colorValue);
     });
-
     $('.prop-control input[type="color"]').change();
+
+    $('#model-template').on('change', function () {
+        var value = $(this).val();
+        $('[data-show-template].active').addClass('leaving').fadeOut('300', function () {
+            $(this).removeClass('active leaving');
+            $('[data-show-template="' + value + '"]').addClass('active entering').show();
+        });
+    });
 
     $('.collapsible h3').on('click', function () {
         var collapsible = $(this).closest('.collapsible'),
@@ -37,6 +107,14 @@ var initCssGenerator = function () {
             target = $('[data-hide-condition="' + condition + '"]'),
             value = $(this).val().toLowerCase();
         value == condition ? target.slideUp('fast') : target.slideDown('fast');
+    });
+
+    $('[data-box]').on('keyup change', function () {
+        var dataBox = $(this).data('box').split(','),
+            selector = dataBox[0],
+            prop = dataBox[1],
+            val = $(this).val();
+        $('#' + selector).css(prop, val);
     });
 }
 
